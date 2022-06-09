696cd2fc9a5fe93fe3caf2a06be0d44d4d22406a
Salatiel Queiroz <salatielsqueiroz@gmail.com>
2022-06-09 16:37:57 -0300

    fix(v1.0.7): unused variables ts interfaces

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
     'plugin:import/typescript',
     'airbnb',
     'airbnb/hooks',
     'prettier',
-    'plugin:react/jsx-runtime'
+    'plugin:react/jsx-runtime',
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
